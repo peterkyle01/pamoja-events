1405c3cd9de7c53c6ef430b59588aea9753ddbea
peterkyle01 <kylepeterkoine4@gmail.com>
2024-03-31 19:50:15 +0300

    fix:ticket not showing image

--- app/tickets/book-a-ticket/page.tsx
+++ app/tickets/book-a-ticket/page.tsx
@@ -18,28 +18,32 @@ async function Ticket({ event }: { event: Tevent }) {
   noStore();
   const image = await readStorage(event.image);
   return (
-    <Link href={`/tickets/book-a-ticket/${event.id}`} key={event.id}>
-      <div className="group h-60 w-full rounded-md border shadow-md duration-500 hover:scale-105">
-        <div className="relative h-3/4 w-full overflow-hidden ">
-          <Image
-            src={image as string}
-            fill
-            alt={event.image}
-            className="object-cover duration-500 group-hover:scale-110"
-          />
-        </div>
-        <div className="grid h-1/4 w-full p-2">
-          <p className="flex items-center gap-2 font-bold tracking-wider text-pm_blue">
-            <FaTicketAlt />
-            {event.name}
-          </p>
-          <p className="flex items-center gap-2 text-sm tracking-tighter text-pm_brown">
-            <MdDateRange size={18} />{" "}
-            {new Date(event.date_and_time).toDateString()}
-          </p>
-        </div>
-      </div>
-    </Link>
+    <>
+      {image && (
+        <Link href={`/tickets/book-a-ticket/${event.id}`} key={event.id}>
+          <div className="group h-60 w-full rounded-md border shadow-md duration-500 hover:scale-105">
+            <div className="relative h-3/4 w-full overflow-hidden ">
+              <Image
+                src={image}
+                fill
+                alt={event.image}
+                className="object-cover duration-500 group-hover:scale-110"
+              />
+            </div>
+            <div className="grid h-1/4 w-full p-2">
+              <p className="flex items-center gap-2 font-bold tracking-wider text-pm_blue">
+                <FaTicketAlt />
+                {event.name}
+              </p>
+              <p className="flex items-center gap-2 text-sm tracking-tighter text-pm_brown">
+                <MdDateRange size={18} />{" "}
+                {new Date(event.date_and_time).toDateString()}
+              </p>
+            </div>
+          </div>
+        </Link>
+      )}
+    </>
   );
 }
 
